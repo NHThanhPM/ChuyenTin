b6a20c7a52e64c1da8a840e3c02fb66a22a418f6
Nguyễn Hữu Thành <thanhpm210@gmail.com>
2025-11-06 15:05:08 +0000

    af

---
+++ hsg/contest6/sinhtest/b1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    freopen("../ATRISET.INP","w",stdout);
+    int n=100000;
+    cout<<n<<'\n';
+    for(int i=0;i<n;i++){
+        cout<<random()%(1000000)<<' ';
+    }
+}
