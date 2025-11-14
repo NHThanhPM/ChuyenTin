6624bfcda0957430b369d01e86461b3bf95f76e1
thanhpm <thanhPM210@gmail.com>
2025-11-14 19:04:49 +0700

    bai tap 13/11/2025 l1

---
+++ hsg/13.11.2025/Chiahetk.cpp
@@ -0,0 +1,43 @@
+/*                        \\
+                           \\
+__________ __      __      __      ___     __ __      __
+    ||     ||      ||     //\\     ||\\    || ||      ||
+    ||     ||      ||    //  \\    || \\   || ||      ||
+    ||     ||======||   //====\\   ||  \\  || ||======||
+    ||     ||      ||  //      \\  ||   \\ || ||      ||
+    ||     ||      || //        \\ ||    \\|| ||      ||
+*/
+#include <bits/stdc++.h>
+using namespace std;
+void init(){
+    #define TASK "Chiahetk"
+    if(!fopen(TASK".inp","r")){
+        return;
+    }
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    freopen(TASK".inp","r",stdin);
+    freopen(TASK".out","w",stdout);
+}
+#define ULL int
+ULL n,k,sum=0,res=0;;
+map<ULL,ULL> SoDu;
+// map<ULL,ULL> sum;
+int main(){
+    init();
+    cin>>n>>k;
+    // SoDu.resize(k,0);
+    SoDu[0]=1;
+    ULL tmp;
+    int*it;
+    for(ULL i=0;i<n;i++){
+        cin>>tmp;
+        sum+=tmp;
+        sum%=k;
+        if(sum<0)sum+=k;
+        it=&SoDu[sum];
+        res+=*it;
+        (*it)++;
+    }
+    cout<<res;
+}
---
+++ hsg/13.11.2025/line.cpp
@@ -0,0 +1,55 @@
+/*                        \\
+                           \\
+__________ __      __      __      ___     __ __      __
+    ||     ||      ||     //\\     ||\\    || ||      ||
+    ||     ||      ||    //  \\    || \\   || ||      ||
+    ||     ||======||   //====\\   ||  \\  || ||======||
+    ||     ||      ||  //      \\  ||   \\ || ||      ||
+    ||     ||      || //        \\ ||    \\|| ||      ||
+*/
+#include <bits/stdc++.h>
+using namespace std;
+void init(){
+    #define TASK "line"
+    if(!fopen(TASK".inp","r")){
+        return;
+    }
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    freopen(TASK".inp","r",stdin);
+    freopen(TASK".out","w",stdout);
+}
+int n,m;
+struct segment{
+    int start,end;
+    friend inline bool operator<(const segment&a,const segment&b){
+        return a.start<b.start ||(a.start==b.start && a.end<b.end);
+    };
+};
+int res=0;
+vector<segment> sms;
+vector<int> p;
+int main(){
+    //đọc và xử lỹ dữ liệu
+    init();
+    cin>>n>>m;
+    sms.resize(n);
+    p.resize(m);
+    for(int i=0;i<n;i++){
+        cin>>sms[i].start>>sms[i].end;
+    }
+    sort(sms.begin(),sms.end());
+    for(int i=0;i<m;i++){
+        cin>>p[i];
+    }
+    sort(p.begin(),p.end());
+    //chạy thuật toán
+    auto left=p.begin();
+    auto right=p.begin();
+    for(int i=0;i<n;i++){
+        left=lower_bound(left,p.end(),sms[i].start);
+        right=upper_bound(right,p.end(),sms[i].end);
+        res+=(right-left)>=2;
+    }
+    cout<<res;
+}
